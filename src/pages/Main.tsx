da7312ced9922ad6aa63d5d8a85d859172083213
dongchurl <churl@zzieut.com>
2023-03-27 22:41:00 +0900

    feat: 프리뷰 화면을 수정합니다

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 import { Typography } from '@components/ui/typography/Typography';
+import { InputCompound } from '@components/ui/input/InputCompound';
+import { useState } from 'react';
 
 export const Main = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChangeInputValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <Container>
       <Typography variant="h1">Hello</Typography>
@@ -19,6 +27,12 @@ export const Main = () => {
       <Typography variant="subtitle1">Hello</Typography>
       <Typography variant="subtitle2">Hello</Typography>
       <Typography variant="overline">Hello</Typography>
+      <InputCompound value={inputValue} type="text" onChange={handleChangeInputValue}>
+        <InputCompound.Container>
+          <InputCompound.Label>이름</InputCompound.Label>
+          <InputCompound.Input />
+        </InputCompound.Container>
+      </InputCompound>
     </Container>
   );
 };
@@ -26,7 +40,7 @@ export const Main = () => {
 const Container = styled.div`
   ${({ theme }) => {
     return css`
-      background-color: ${theme.palette.primary};
+      background-color: ${theme.palette.white};
       display: flex;
       width: 100vw;
       height: 100vh;
