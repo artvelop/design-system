31452f44376521d9635659ba7a3ec90ed371c20d
artvelop <richcode99@gmail.com>
2023-05-12 13:09:44 +0900

    feat: recoil 환경 셋팅 합니다

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from '@components/theme/ThemeProvider';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>,
 );
 
